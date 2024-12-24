0cf29308883cde51747beac150041ebd10e73529
Surendra Dire <83612581+surendradire@users.noreply.github.com>
2024-12-24 10:01:31 +0100

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -2,17 +2,17 @@
 # Script: jenkins.sh
 # Purpose: Install jenkins
 # Steps: 
-# Update package list and upgrade all installed packages to the latest versions
-# Install OpenJDK 17 runtime environment (JRE)
-# Add Jenkins key and repository
-# Install Jenkins
-# Date: 25-7-2023
+  # 1. Update package list and upgrade all installed packages to the latest versions
+  # 2. Install OpenJDK 17 runtime environment (JRE)
+  # 3. Add Jenkins key and repository
+  # 4. Update package list to include Jenkins repository
+  # 5. Install Jenkins
 ###########################################################################
-
 #!/bin/bash
+
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install openjdk-17-jre -y
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt update -y            
-sudo apt install jenkins -y    
+sudo apt update -y
+sudo apt install jenkins -y
